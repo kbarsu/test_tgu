b040d8c867439806046a2131f9cd3f2f1f51e541
V.E <kbarsu@ya.ru>
2020-03-20 14:13:32 +0400

    Ещё один вариант

--- SQL_exepmle_add.sql
+++ SQL_exepmle_add.sql
@@ -6,11 +6,11 @@ create or replace type t_tab as table of t_rec;
 /
 
 -- табличная функция 
-function f_tab(p_start date, p_end date) return t_tab 
-  is
-  r_val t_tab := t_tab(); 
+create or replace function f_tab (p_start date, p_end date)
+return t_tab pipelined is
 begin
  for i in (
+  -- запрос для получения списка дат в указанном периоде
   select distinct 
   case
     when bd = p_start then bd
@@ -22,21 +22,54 @@ begin
     when bd = p_end then bd
     else p_end
   end t_end
-      from (
-        select p_start+level-1 bd 
-        from dual connect by level <= p_end 
-        - p_start
-      ) where bd between p_start and p_end order by 1
+  from (
+      select p_start+level-1 bd 
+      from dual connect by level <= p_end 
+      - p_start
+  ) where bd between p_start and p_end order by 1
+
     )
  loop
-   r_val.f_start := i.t_start;
-   r_val.f_end := i.t_end;
-   retval.EXTEND;
-   retval(retval.LAST) := r_val;
- end loop;
-return r_val;
+    -- запонняем табличный тип 
+    pipe row(t_rec(i.t_start,i.t_end));
+  end loop;
+  return;
 end;
 /
 
 -- вызов в предложении SELECT 
-select * from table(f_tab(to_date('15.02.2020','dd.mm.yyyy'),to_date('29.04.2020','dd.mm.yyyy')))
+select * from table(f_tab(to_date('15.02.2020','dd.mm.yyyy'),to_date('29.04.2020','dd.mm.yyyy')));
+
+
+------------------------------
+-- табличная функция варинат 2
+------------------------------
+create or replace function f_tab_1 (p_start date, p_end date)
+return t_tab pipelined is
+m_def number;
+begin
+ m_def := to_number(to_char(p_end,'mm')) - to_number(to_char(p_start,'mm')) + 1; 
+ for i in (
+  -- запрос для получения списка дат в указанном периоде
+  select 
+  case
+    when level = 1 then p_start
+    else add_months(trunc(p_start,'mm'), level - 1)    
+  end t_start,
+  case
+    when level = 1 then last_day(p_start)
+    when level = m_def then p_end
+    else last_day(add_months(trunc(p_start,'mm'), level - 1))
+  end t_end
+  from dual connect by level <= m_def
+    )
+ loop
+    -- запонняем табличный тип 
+    pipe row(t_rec(i.t_start,i.t_end));
+  end loop;
+  return;
+end;
+/
+
+-- вызов в предложении SELECT 
+select * from table(f_tab_1(to_date('15.02.2020','dd.mm.yyyy'),to_date('28.06.2020','dd.mm.yyyy')));
